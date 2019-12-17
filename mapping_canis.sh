456961faf048527562d0762a99cc511e8687b63d
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-17 20:05:42 +0100

    Update mapping_canis.sh

--- mapping_canis.sh
+++ mapping_canis.sh
@@ -1,3 +1,24 @@
+# -------------------------------------------------------------------------------
+# making the index
+
 GENOME=GCA_003254725.1_ASM325472v1_genomic.fna
 
 ./STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir test/genome/ --genomeFastaFiles ${GENOME}
+
+# -------------------------------------------------------------------------------
+# mapping the reads
+
+for A in $(ls raw_reads/raw_reads/*fastq | sort | grep "" | grep "_R1_")
+do
+        echo $A
+        B=$(ls $A | sed 's/_R1_/_R2_/g')
+        echo $B
+        C=$(ls $A | sed 's/_R1_/_RX_/g')
+        echo $C
+        ls -al $A
+        ls -al $B
+        echo
+        ./STAR-2.7.3a/bin/Linux_x86_64/STAR --genomeDir canis/test/genome --readFilesIn $A $B
+done
+# -------------------------------------------------------------------------------
+
