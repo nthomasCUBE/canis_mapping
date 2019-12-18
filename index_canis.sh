708ef471b40ed43fe13331a7f4a294b0811b6a3e
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-18 20:41:10 +0100

    Create index_canis.sh

---
+++ index_canis.sh
@@ -0,0 +1,8 @@
+GENOME=GCA_003254725.1_ASM325472v1_genomic.fna
+
+./STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir test/genome/ --genomeFastaFiles ${GENOME} --limitGenomeGenerateRAM 16000000000
+
+
+
+
+
