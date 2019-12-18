6b13c1ab89c969b1819dc5ce43f1a9418b02ebb3
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-18 20:40:27 +0100

    Update mapping_canis.sh

--- mapping_canis.sh
+++ mapping_canis.sh
@@ -1,12 +1,7 @@
-# -------------------------------------------------------------------------------
-# making the index
+rm Aligned.out.sam
+rm ReadsPerGene.out.tab
 
-GENOME=GCA_003254725.1_ASM325472v1_genomic.fna
-
-./STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir test/genome/ --genomeFastaFiles ${GENOME}
-
-# -------------------------------------------------------------------------------
-# mapping the reads
+gtfreference="Canis_lupus_dingo.ASM325472v1.98.gtf"
 
 for A in $(ls raw_reads/raw_reads/*fastq | sort | grep "" | grep "_R1_")
 do
@@ -15,10 +10,17 @@ do
         echo $B
         C=$(ls $A | sed 's/_R1_/_RX_/g')
         echo $C
+        C=$(basename $C)
         ls -al $A
         ls -al $B
-        echo
-        ./STAR-2.7.3a/bin/Linux_x86_64/STAR --genomeDir canis/test/genome --readFilesIn $A $B
+
+        tmp1=$(mktemp)
+        tmp2=$(mktemp)
+        head -n 10000 $A > $tmp1
+        head -n 10000 $B > $tmp2
+
+        ./STAR-2.7.3a/bin/Linux_x86_64/STAR --genomeDir canis/test/genome --quantMode GeneCounts --sjdbGTFfile $gtfreference --readFilesIn $tmp1 $tmp2
+        mv Aligned.out.sam ${C}.sam
+        mv ReadsPerGene.out.tab ${C}_ReadsPerGene.tab
 done
-# -------------------------------------------------------------------------------
 
