c1fb8a65c9d3d6e4597c2d929f28713753e94a5a
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-19 19:56:42 +0100

    Update normalize.sh

--- normalize.sh
+++ normalize.sh
@@ -1,14 +1,17 @@
 library(edgeR)
 
-x <- read.delim("h1_merge_counts.tab", stringsAsFactors=FALSE)
+x = read.delim("h1_merge_counts_counts.txt", stringsAsFactors=FALSE)
 group <- 2:dim(x)[2]
 genes <- x[,1]
+
+x2 = read.csv("h1_merge_counts_length.txt",sep="\t")
 
 my_g=x[,2:dim(x)[2]]
 rownames(my_g)=x[,1]
 y <- DGEList(counts=my_g, group=group, genes=genes)
-y <- calcNormFactors(y, method="TMM")
-write.table(cpm(y),"normalize_19dec19.tab",sep="\t",quote=FALSE)
-y <- estimateCommonDisp(y)
+y$genes$Length=x2[,2]
+mycounts <- rpkm(y)
+write.table(mycounts, file = "rpkm.txt", dec = ".", sep = "\t", quote = FALSE)
+
 
 
