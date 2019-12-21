03094af993b9d3babef7c18ef64e2666a88db6e0
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-21 15:34:37 +0100

    Update and rename index_canis.sh to index.sh

--- index.sh
+++ index.sh
@@ -1,8 +1,3 @@
 GENOME=GCA_003254725.1_ASM325472v1_genomic.fna
 
 ./STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir test/genome/ --genomeFastaFiles ${GENOME} --limitGenomeGenerateRAM 16000000000
-
-
-
-
-
