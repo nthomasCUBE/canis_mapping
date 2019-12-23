5b50c656a491bd50fa528b7dd74e04617c38e8ce
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-23 08:39:51 +0100

    Update cqn.R

--- cqn.R
+++ cqn.R
@@ -1,16 +1,16 @@
-library(EDASeq)
 library(cqn)
 
 options(stringsAsFactors=FALSE)
+options(digits=2)
 
 L=read.csv("h1_merge_counts_length.txt",sep="\t",header=T)
 
 Data <- as.matrix(read.table("h1_merge_counts_counts.txt",sep="\t",header=TRUE,row.names=1, check.names=F))
 
-my_x=rep(1,dim(Data)[1])
-my_x=sample(50:80,length(my_x),replace=TRUE)
+my_x=L[,4]
 
 cqn.subset <- cqn(Data,x=my_x,lengths=L[,2])
-RPKM.cqn=cqn.subset$y+cqn.subset$offset
-
+cqn_norm=cqn.subset$y+cqn.subset$offset
+write.table(cqn_norm,"cqn_23dec19.txt",sep="\t",quote=F)
+write.table(cqn.subset$y,"cqn_no_offset_23dec19.txt",sep="\t",quote=F)
 
