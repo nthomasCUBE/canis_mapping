ad14f3c0c40bd8fc7053735dc37ad379ff601cf9
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-24 10:49:32 +0100

    Update prep_count.sh

--- prep_count.sh
+++ prep_count.sh
@@ -1,10 +1,26 @@
 rm -r COUNT2
 mkdir COUNT2
 
-for FILE in $(ls SAM/*)
-do
-	echo $FILE
-	BN=$(basename $FILE)
-	python DEXSeq-master/inst/python_scripts/dexseq_count.py --paired yes Canis_lupus_dingo.ASM325472v1.98_flattened.gff3 ${FILE} COUNT2/${BN}.count
-done
+GENOME=$1
+
+if [ $GENOME == "familaris" ]
+then
+        for FILE in $(ls canis/SAM/*)
+        do
+                echo $FILE
+                BN=$(basename $FILE)
+                python DEXSeq-master/inst/python_scripts/dexseq_count.py --paired yes Canis_familiaris.CanFam3.1.98.flattened.gff3 ${FILE} COUNT2/${BN}.count
+        done
+elif [ $GENOME == "dingo" ]
+then
+        for FILE in $(ls SAM/*)
+        do
+                echo $FILE
+                BN=$(basename $FILE)
+                python DEXSeq-master/inst/python_scripts/dexseq_count.py --paired yes Canis_lupus_dingo.ASM325472v1.98_flattened.gff3 ${FILE} COUNT2/${BN}.count
+        done
+fi
+
+
+
 
