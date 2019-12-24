a4a9e3b32191904ae9b119fbfee8ed785ea8e246
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-12-24 10:41:04 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,3 +1,22 @@
-GENOME=GCA_003254725.1_ASM325472v1_genomic.fna
+GENOME=$1
 
-./STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir test/genome/ --genomeFastaFiles ${GENOME} --limitGenomeGenerateRAM 16000000000
+if [ $GENOME == "familaris" ]
+then
+        echo "familaris"
+        GENOME=Canis_familiaris.CanFam3.1.dna_sm.toplevel.fa
+elif [ $GENOME == "dingo" ]
+then
+        echo "dingo"
+        GENOME=GCA_003254725.1_ASM325472v1_genomic.fna
+fi
+
+if [ $1 == "familaris" ] || [ $1 == "dingo" ]
+then
+        echo "Mapping"
+        ../canis/STAR-2.7.3a/bin/Linux_x86_64/STAR --runMode genomeGenerate --genomeDir index --genomeFastaFiles ${GENOME} --limitGenomeGenerateRAM 16000000000
+fi
+
+
+
+
+
