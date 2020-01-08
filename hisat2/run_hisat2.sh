a57b0dd487ab4b8bf09f22372b9c724254f193cf
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2020-01-08 20:56:26 +0100

    Update run_hisat2.sh

--- hisat2/run_hisat2.sh
+++ hisat2/run_hisat2.sh
@@ -9,17 +9,21 @@ SHORT_NAME=$(echo $GENOME | sed 's/\./ /g' | cut -f 1 -d " ")
 
 echo "starting hisat2 index"
 
-hisat2-build ${GENOME} ${SHORT_NAME}
+#hisat2-build ${GENOME} ${SHORT_NAME}
 
 echo "end hisat2 index"
 
 
 echo "starting hisat2 mapping"
 
+head -n 10000 ../_old/canis/raw_reads/raw_reads/1033_ACAGTG_L006_R1_20M.fastq > tmp1.fastq
+head -n 10000 ../_old/canis/raw_reads/raw_reads/1033_ACAGTG_L006_R1_20M.fastq > tmp2.fastq
+
+hisat2 -x ${SHORT_NAME} -1 tmp1.fastq -2 tmp2.fastq -S tmp12.sam
+
 echo "end hisat2 mapping"
 
 echo "-------------------------------------------------------------"
-
 
 
 
