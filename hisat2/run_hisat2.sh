9e58aa67fe46c7f0d765df2d2c3ba021d8f14512
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2020-01-08 21:01:47 +0100

    Update run_hisat2.sh

--- hisat2/run_hisat2.sh
+++ hisat2/run_hisat2.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+echo "-------------------------------------------------------------"
+
 PATH=hisat2-2.1.0:$PATH
 
 echo "-------------------------------------------------------------"
@@ -13,17 +15,18 @@ echo "starting hisat2 index"
 
 echo "end hisat2 index"
 
+echo "-------------------------------------------------------------"
 
 echo "starting hisat2 mapping"
 
-head -n 10000 ../_old/canis/raw_reads/raw_reads/1033_ACAGTG_L006_R1_20M.fastq > tmp1.fastq
-head -n 10000 ../_old/canis/raw_reads/raw_reads/1033_ACAGTG_L006_R1_20M.fastq > tmp2.fastq
+date
 
-hisat2 -x ${SHORT_NAME} -1 tmp1.fastq -2 tmp2.fastq -S tmp12.sam
+head -n 10000 ../_old/canis/raw_reads/raw_reads/1033_ACAGTG_L006_R1_20M.fastq > 1033_1.fastq
+head -n 10000 ../_old/canis/raw_reads/raw_reads/1033_ACAGTG_L006_R2_20M.fastq > 1033_2.fastq
+
+hisat2 -p 16 -x ${SHORT_NAME} -1 1033_1.fastq -2 1033_2.fastq -S 1033.sam
 
 echo "end hisat2 mapping"
 
-echo "-------------------------------------------------------------"
-
-
+date
 
